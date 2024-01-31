8014c52eff354eed67a6193aae7fba14236d0e06
Trey <treyoung12@yahoo.com>
2024-01-30 23:57:24 -0500

    Setting region vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
+  default = "us-east-1"
   description = "AWS Region"
   type = string
 }
 
 variable "region-west"{
+  default = "us-west-1"
   description = "AWS Region for west"
   type = string
 }
